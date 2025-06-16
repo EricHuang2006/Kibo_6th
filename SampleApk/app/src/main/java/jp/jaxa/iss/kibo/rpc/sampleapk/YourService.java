c1ad39326710031ebdb2227e234e6e511658afd5
phoenix0423 <ericabcd423@gmail.com>
2025-06-16 23:00:59 +0800

    add logPose

--- SampleApk/app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/YourService.java
+++ SampleApk/app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/YourService.java
@@ -13,108 +13,49 @@ import org.opencv.core.Mat;
 
 public class YourService extends KiboRpcService {
     @Override
-    protected void runPlan1() {
-        // 1. 開始任務
+    protected void runPlan1(){
+        // The mission starts.
         api.startMission();
 
-        // 2. 巡視第一個區域
-        moveToArea(1);
-        String item1 = identifyItem(1);
+        // Move to a point.
+        Point point = new Point(10.9d, -9.92284d, 5.195d);
+        Quaternion quaternion = new Quaternion(0f, 0f, -0.707f, 0.707f);
+        api.moveTo(point, quaternion, false);
 
-        // 3. 巡視第二個區域
-        moveToArea(2);
-        String item2 = identifyItem(2);
+        // Get a camera image.
+        Mat image = api.getMatNavCam();
 
-        // 4. 巡視第三個區域
-        moveToArea(3);
-        String item3 = identifyItem(3);
+        /* ******************************************************************************** */
+        /* Write your code to recognize the type and number of landmark items in each area! */
+        /* If there is a treasure item, remember it.                                        */
+        /* ******************************************************************************** */
 
-        // 5. 巡視第四個區域
-        moveToArea(4);
-        String item4 = identifyItem(4);
+        // When you recognize landmark items, let’s set the type and number.
+        api.setAreaInfo(1, "item_name", 1);
 
-        // 6. 移動到宇航員位置報告完成
-        moveToAstronaut();
+        /* **************************************************** */
+        /* Let's move to each area and recognize the items. */
+        /* **************************************************** */
+
+        // When you move to the front of the astronaut, report the rounding completion.
+        point = new Point(11.143d, -6.7607d, 4.9654d);
+        quaternion = new Quaternion(0f, 0f, 0.707f, 0.707f);
+        api.moveTo(point, quaternion, false);
         api.reportRoundingCompletion();
 
-        // 7. 獲取目標物品資訊
-        String targetItem = identifyTargetItem();
+        /* ********************************************************** */
+        /* Write your code to recognize which target item the astronaut has. */
+        /* ********************************************************** */
+
+        // Let's notify the astronaut when you recognize it.
         api.notifyRecognitionItem();
 
-        // 8. 找出目標物品位置
-        int targetArea = findTargetItemArea(targetItem);
+        /* ******************************************************************************************************* */
+        /* Write your code to move Astrobee to the location of the target item (what the astronaut is looking for) */
+        /* ******************************************************************************************************* */
 
-        // 9. 移動到目標物品位置
-        moveToArea(targetArea);
-
-        // 10. 拍攝目標物品並完成任務
+        // Take a snapshot of the target item.
         api.takeTargetItemSnapshot();
-    }
-
-    // 移動到指定區域
-    private void moveToArea(int areaId) {
-        Point point;
-        Quaternion quaternion = new Quaternion(0, 0, -0.707f, 0.707);
-
-        switch (areaId) {
-            case 1:
-                point = new Point(10.9, -9.9, 5.2);
-                break;
-            case 2:
-                point = new Point(10.9, -8.9, 4.8);
-                break;
-            case 3:
-                point = new Point(10.9, -7.9, 4.8);
-                break;
-            case 4:
-                point = new Point(10.9, -6.9, 5.2);
-                break;
-            default:
-                return;
-        }
-
-        api.moveTo(point, quaternion, false);
-    }
-
-    // 識別區域中的物品
-    private String identifyItem(int areaId) {
-        Mat image = api.getMatNavCam();
-
-        // 圖像處理邏輯（示例）
-        String itemName = "unknown";
-        int itemCount = 0;
-
-        // 根據圖像識別結果設置物品名稱和數量
-        // 這裡應該是您的圖像處理邏輯
-
-        // 報告識別結果
-        api.setAreaInfo(areaId, itemName, itemCount);
-
-        return itemName;
-    }
-
-    // 移動到宇航員位置
-    private void moveToAstronaut() {
-        Point astronautPoint = new Point(11.2, -7.0, 5.0);
-        Quaternion quaternion = new Quaternion(0, 0, 0.707f, 0.707f);
-        api.moveTo(astronautPoint, quaternion, false);
-    }
-
-    // 識別目標物品
-    private String identifyTargetItem() {
-        Mat image = api.getMatNavCam();
-
-        // 目標物品識別邏輯（示例）
-        String targetItemName = "emerald";
-
-        return targetItemName;
-    }
-
-    // 找出目標物品所在區域
-    private int findTargetItemArea(String targetItem) {
-        // 根據之前識別的結果找出目標物品所在區域
-        // 這裡應該是您的邏輯
-        return 2;  // 示例返回值
     }
 
     @Override
--- TemplateApk/app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
+++ TemplateApk/app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
@@ -1,126 +1,395 @@
-package jp.jaxa.iss.kibo.rpc.defaultapk;
+package jp.jaxa.iss.kibo.rpc.sampleapk;
 
 import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
 
+import gov.nasa.arc.astrobee.types.Point;
+import gov.nasa.arc.astrobee.types.Quaternion;
+
+import org.opencv.core.Mat;
+
+// new imports
+import android.util.Log;
+
+import java.util.List;
+import java.util.ArrayList;
+
+
+// new OpenCV imports
+import org.opencv.aruco.Dictionary;
+import org.opencv.aruco.Aruco;
+import org.opencv.calib3d.Calib3d;
+import org.opencv.core.CvType;
+import java.io.InputStream;
+import java.io.IOException;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import org.opencv.android.Utils;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.core.Size;
+import org.opencv.core.Core;
+
+
 /**
- * Class meant to handle commands from the Ground Data System and execute them in Astrobee
+ * Class meant to handle commands from the Ground Data System and execute them in Astrobee.
  */
 
 public class YourService extends KiboRpcService {
+
+    // The TAG is used for logging.
+    // You can use it to check the log in the Android Studio.
+    private final String TAG = this.getClass().getSimpleName();
+
+    // Type all the image in the template folder.
+    //'coin.png', 'compass.png', 'coral.png', 'crystal.png',
+    // 'diamond.png', 'emerald.png', 'fossil.png', 'key.png',
+    // 'letter.png', 'shell.png', 'treasure_box.png']
+    private final String[] TEMPLATE_FILE_NAMES = {
+            "coin.png",
+            "compass.png",
+            "coral.png",
+            // "crystal.png",
+            // "diamond.png",
+            // "emerald.png",
+            "fossil.png",
+            "key.png",
+            "letter.png",
+            "shell.png",
+            "treasure_box.png"
+    };
+
+    private final String[] TEMPLATE_NAMES = {
+            "coin",
+            "compass",
+            "coral",
+            // "crystal",
+            // "diamond",
+            // "emerald",
+            "fossil",
+            "key",
+            "letter",
+            "shell",
+            "treasure_box"
+    };
+
+    private final Point[] AREA_POINTS = {
+            new Point(10.9d, -10.0000d, 5.195d),    // Area 1
+            new Point(10.925d, -8.875d, 4.602d),    // Area 2
+            new Point(10.925d, -7.925d, 4.60093d),    // Area 3
+            new Point(10.766d, -6.852d, 4.945d)     // Area 4
+    };
+
+    private final Quaternion[] AREA_QUATERNIONS = {
+            new Quaternion(0f, 0f, -0.707f, 0.707f),    // Area 1
+            new Quaternion(0f, 0.707f, 0f, 0.707f),     // Area 2
+            new Quaternion(0f, 0.707f, 0f, 0.707f),     // Area 3
+            new Quaternion(0f, 0f, 1f, 0f)      // Area 4
+    };
+
     @Override
-    protected void runPlan1() {
-        // 1. 開始任務
+    protected void runPlan1(){
+        // Log the start of the mission.
+        Log.i(TAG, "Start mission");
+        // The mission starts.
         api.startMission();
 
-        // 2. 巡視第一個區域
-        moveToArea(1);
-        String item1 = identifyItem(1);
+        // Prepare the variables to store treasure item locations
+        String[] foundItems = new String[AREA_POINTS.length];
+        int[] itemCounts = new int[AREA_POINTS.length];
 
-        // 3. 巡視第二個區域
-        moveToArea(2);
-        String item2 = identifyItem(2);
+        // Load template images
+        Mat[] templates = new Mat[TEMPLATE_FILE_NAMES.length];
+        for (int i = 0; i < TEMPLATE_FILE_NAMES.length; i++) {
+            try{
+                // Open the template image file in Bitmap from the file name and convert to Mat
+                InputStream inputStream = getAssets().open(TEMPLATE_FILE_NAMES[i]);
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                Mat mat = new Mat();
+                Utils.bitmapToMat(bitmap, mat);
 
-        // 4. 巡視第三個區域
-        moveToArea(3);
-        String item3 = identifyItem(3);
+                // Convert the Bitmap to grayscale
+                Imgproc.cvtColor(mat, mat, Imgproc.COLOR_BGR2GRAY);
 
-        // 5. 巡視第四個區域
-        moveToArea(4);
-        String item4 = identifyItem(4);
+                // Assign to an array of templates
+                templates[i] = mat;
 
-        // 6. 移動到宇航員位置報告完成
-        moveToAstronaut();
-        api.reportRoundingCompletion();
+                // Release the InputStream
+                inputStream.close();
 
-        // 7. 獲取目標物品資訊
-        String targetItem = identifyTargetItem();
-        api.notifyRecognitionItem();
-
-        // 8. 找出目標物品位置
-        int targetArea = findTargetItemArea(targetItem);
-
-        // 9. 移動到目標物品位置
-        moveToArea(targetArea);
-
-        // 10. 拍攝目標物品並完成任務
-        api.takeTargetItemSnapshot();
-    }
-
-    // 移動到指定區域
-    private void moveToArea(int areaId) {
-        Point point;
-        Quaternion quaternion = new Quaternion(0, 0, -0.707, 0.707);
-
-        switch (areaId) {
-            case 1:
-                point = new Point(10.9, -9.9, 5.2);
-                break;
-            case 2:
-                point = new Point(10.9, -8.9, 4.8);
-                break;
-            case 3:
-                point = new Point(10.9, -7.9, 4.8);
-                break;
-            case 4:
-                point = new Point(10.9, -6.9, 5.2);
-                break;
-            default:
-                return;
+            } catch (IOException e) {
+                Log.e(TAG, "Error loading template image: " + TEMPLATE_FILE_NAMES[i], e);
+                e.printStackTrace();
+            }
         }
 
-        api.moveTo(point, quaternion, false);
-    }
+        // Visit each area
+        for (int areaId = 0; areaId < AREA_POINTS.length; areaId++) {
+            // Move to the current area
+            api.moveTo(AREA_POINTS[areaId], AREA_QUATERNIONS[areaId], false);
+            logPose();
+            // Get a camera image
+            Mat image = api.getMatNavCam();
+            // Save the image for debugging
+            api.saveMatImage(image, "area_" + (areaId + 1) + ".png");
 
-    // 識別區域中的物品
-    private String identifyItem(int areaId) {
-        Mat image = api.getMatNavCam();
+            /* ********************************************** */
+            /* Start image recognition process */
+            /* ********************************************** */
+            // AR tag detection
+            Dictionary dictionary = Aruco.getPredefinedDictionary(Aruco.DICT_5X5_250);
+            List<Mat> corners = new ArrayList<>();
+            Mat ids = new Mat();
+            Aruco.detectMarkers(image, dictionary, corners, ids);
 
-        // 圖像處理邏輯（示例）
-        String itemName = "unknown";
-        int itemCount = 0;
+            // Undistort the image
+            Mat cameraMatrix = new Mat(3,3,CvType.CV_64F);
+            cameraMatrix.put(0, 0, api.getNavCamIntrinsics()[0]);
 
-        // 根據圖像識別結果設置物品名稱和數量
-        // 這裡應該是您的圖像處理邏輯
+            Mat cameraCoefficients = new Mat(1, 5, CvType.CV_64F);
+            cameraCoefficients.put(0, 0, api.getNavCamIntrinsics()[1]);
+            cameraCoefficients.convertTo(cameraCoefficients, CvType.CV_64F);
 
-        // 報告識別結果
-        api.setAreaInfo(areaId, itemName, itemCount);
+            Mat undistortImg = new Mat();
+            Calib3d.undistort(image, undistortImg, cameraMatrix, cameraCoefficients);
 
-        return itemName;
-    }
+            // Number of matches for each template
+            int[] numMatches = new int[TEMPLATE_FILE_NAMES.length];
 
-    // 移動到宇航員位置
-    private void moveToAstronaut() {
-        Point astronautPoint = new Point(11.2, -7.0, 5.0);
-        Quaternion quaternion = new Quaternion(0, 0, 0.707, 0.707);
-        api.moveTo(astronautPoint, quaternion, false);
-    }
+            // Get the number of template matches
+            for (int tempNum = 0; tempNum < templates.length; tempNum++) {
+                // number of matches
+                int matchCount = 0;
+                // Coordinates of the match location
+                List<org.opencv.core.Point> matchLocations = new ArrayList<>();
 
-    // 識別目標物品
-    private String identifyTargetItem() {
-        Mat image = api.getMatNavCam();
+                // Load the template image
+                Mat template = templates[tempNum].clone();
+                // Target image is the undistorted image
+                Mat targetImg = undistortImg.clone();
 
-        // 目標物品識別邏輯（示例）
-        String targetItemName = "emerald";
+                // Pattern matching
+                int widthMin = 20; //[px]
+                int widthMax = 100; //[px]
+                int changeWidth = 5; //[px]
+                int changeAngle = 45; //[px]
 
-        return targetItemName;
-    }
+                for (int size=widthMin; size <= widthMax; size += changeWidth){
+                    for (int angle=0; angle<360; angle+=changeAngle){
+                        // Resize the template image
+                        Mat resizedTemplate = scalingresizeImage(template, size);
+                        // Rotate the template image
+                        Mat rotatedTemplate = rotImg(resizedTemplate, angle);
 
-    // 找出目標物品所在區域
-    private int findTargetItemArea(String targetItem) {
-        // 根據之前識別的結果找出目標物品所在區域
-        // 這裡應該是您的邏輯
-        return 2;  // 示例返回值
+                        // Perform template matching
+                        Mat result = new Mat();
+                        Imgproc.matchTemplate(targetImg, rotatedTemplate, result, Imgproc.TM_CCOEFF_NORMED);
+
+                        // Thresholding
+                        double threshold = 0.7;
+                        Core.MinMaxLocResult mmlr = Core.minMaxLoc(result);
+
+                        double maxVal = mmlr.maxVal;
+
+                        if (maxVal >= threshold) {
+                            // Create a mask for the detected region
+                            Mat thresholdResult = new Mat();
+                            Imgproc.threshold(result, thresholdResult, threshold, 1, Imgproc.THRESH_TOZERO);
+
+                            // Get coordinates of the detected region
+                            for(int y=0; y<thresholdResult.rows(); y++){
+                                for(int x=0; x<thresholdResult.cols(); x++){
+                                    if(thresholdResult.get(y, x)[0] > 0){
+                                        matchLocations.add(new org.opencv.core.Point(x, y));
+                                    }
+                                }
+                            }
+
+                            // Release resources
+                            thresholdResult.release();
+                        }
+
+                        // Release resources
+                        result.release();
+                        rotatedTemplate.release();
+                        resizedTemplate.release();
+                    }
+                }
+
+                List<org.opencv.core.Point> filteredMatches = removeDuplicates(matchLocations);
+                matchCount += filteredMatches.size();
+
+                // Number of matches
+                numMatches[tempNum] = matchCount;
+
+                // Release resources
+                template.release();
+                targetImg.release();
+            }
+
+            // Find the most matched template
+            int mostMatchTemplateNum = getMxIndex(numMatches);
+
+            // Store the results
+            foundItems[areaId] = TEMPLATE_NAMES[mostMatchTemplateNum];
+            itemCounts[areaId] = numMatches[mostMatchTemplateNum];
+            /* ********************************************** */
+            /* End image recognition process  and Reprot*/
+            // Report what was found in this area
+            api.setAreaInfo(areaId + 1, foundItems[areaId], itemCounts[areaId]);
+
+            // Log the results for debugging
+            Log.i(TAG, "Area " + (areaId + 1) + ": Found " + itemCounts[areaId] + " of " + foundItems[areaId]);
+
+            // Release resources
+            image.release();
+            undistortImg.release();
+            cameraMatrix.release();
+            cameraCoefficients.release();
+            ids.release();
+            for (Mat corner : corners) {
+                corner.release();
+            }
+
+        }
+
+        // Move to the astronaut for reporting
+        Point astronautPoint = new Point(11.143, -6.7607, 4.9654);
+        Quaternion astronautQuaternion = new Quaternion(0f, 0f, 0.707f, 0.707f);
+        api.moveTo(astronautPoint, astronautQuaternion, false);
+        api.reportRoundingCompletion();
+
+        // Get information about the target item from the astronaut
+        // Mat targetImage = api.getMatNavCam();
+        // api.saveMatImage(targetImage, "target_item.png");
+
+        // TODO: Implement logic to identify the target item from astronaut's information
+        // For now, let's assume the target is in the first area
+        // String targetItem = TEMPLATE_NAMES[0]; // Replace with actual target item identification
+
+        // Notify that we recognized the target item
+        api.notifyRecognitionItem();
+
+        // // Find which area contains the target item and move there
+        // int targetArea = 0; // Default to first area if not found
+        // for (int i = 0; i < foundItems.length; i++) {
+        //     if (foundItems[i].equals(targetItem)) {
+        //         targetArea = i;
+        //         break;
+        //     }
+        // }
+
+        // Move to the area with the target item
+        // api.moveTo(AREA_POINTS[targetArea], AREA_QUATERNIONS[targetArea], false);
+
+        // Take a snapshot of the target item
+        api.takeTargetItemSnapshot();
+
+        // Release resources
+        // targetImage.release();
+        // for (Mat template : templates) {
+        //     if (template != null) {
+        //         template.release();
+        //     }
+        // }
     }
 
     @Override
     protected void runPlan2(){
-        // write your plan 2 here
+        // write your plan 2 here.
     }
 
     @Override
     protected void runPlan3(){
-        // write your plan 3 here
+        // write your plan 3 here.
     }
 
-}
+    private Mat scalingresizeImage(Mat image, int width) {
+        int height = (int) ((double) image.rows() / image.cols() * width);
+        // Create a new Mat object to hold the resized image
+        Mat resizedImage = new Mat();
 
+        // Resize the image using the specified width and height
+        Imgproc.resize(image, resizedImage, new Size(width, height));
+
+        return resizedImage;
+    }
+
+    private Mat rotImg(Mat img, int angle) {
+        // Get the center of the image
+        org.opencv.core.Point center = new org.opencv.core.Point(img.cols() / 2, img.rows() / 2);
+
+        // Create a rotation matrix
+        Mat rotMat = Imgproc.getRotationMatrix2D(center, angle, 1.0);
+
+        // Create a new Mat object to hold the rotated image
+        Mat rotatedImg = new Mat();
+
+        // Rotate the image using the rotation matrix
+        Imgproc.warpAffine(img, rotatedImg, rotMat, img.size());
+
+        // Release resources
+        rotMat.release();
+
+        return rotatedImg;
+    }
+
+    private List<org.opencv.core.Point> removeDuplicates(List<org.opencv.core.Point> points) {
+        double length = 10; // within 10 px
+        List<org.opencv.core.Point> uniquePoints = new ArrayList<>();
+        for (org.opencv.core.Point point : points) {
+            boolean isIncluded = false;
+            for (org.opencv.core.Point uniquePoint : uniquePoints) {
+                double distance = calculateDistance(point, uniquePoint);
+
+                if (distance <= length){
+                    isIncluded = true;
+                    break;
+                }
+            }
+
+            if (!isIncluded) {
+                uniquePoints.add(point);
+            }
+        }
+        return uniquePoints;
+    }
+
+    private double calculateDistance(org.opencv.core.Point p1, org.opencv.core.Point p2) {
+        // Calculate the distance between two points using the Euclidean distance formula
+        return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+    }
+
+    private int getMxIndex(int[] array){
+        int max = 0;
+        int maxIndex = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
+    }
+
+    private void logPosition(){
+        Point p = api.getRobotPosition();
+        double x = p.getX();
+        double y = p.getY();
+        double z = p.getZ();
+        Log.i(TAG, "Current Position: (" + x + ", " + y + ", " + z + ", " + ")" );
+    }
+
+    private void logQuaternion(){
+        Quaternion q = api.getRobotOrientation();
+        double x = q.getX();
+        double y = q.getY();
+        double z = q.getZ();
+        double w = q.getW();
+        Log.i(TAG, "Current Orientation: (" + x + ", " + y + ", " + z + ", " + w + ")" );
+    }
+
+    private void logPose(){
+        logPosition();
+        logQuaternion();
+    }
+}
